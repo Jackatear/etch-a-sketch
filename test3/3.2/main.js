ee5a5976f7c64980d711c77696c9351ccf78658b
Jackatear <jackatear@gmail.com>
2022-12-12 18:06:41 +0000

    test3/3.2 updated the grid slider script so that you get a full grid depending on the value of the slider. Will update later to include the styles in the css as classes that are added in javascript

---
+++ test3/3.2/main.js
@@ -0,0 +1,83 @@
+let slider = document.getElementById("myRange");
+let output = document.createElement('div');
+let container = document.querySelector('.container');
+let x;
+let newDiv, innerDiv;
+
+output.classList.add("value");
+
+
+container.appendChild(output);
+
+x = slider.value;
+
+
+for (let i = 0; i < x; i++){
+    newDiv = document.createElement("div");
+    newDiv.id = 'grid';
+    newDiv.style.height = '10px';
+    newDiv.style.boxSizing = 'border-box';
+    newDiv.style.width = '10px';
+    newDiv.style.border = '0.5px solid black';
+    newDiv.style.flex = '1';
+    newDiv.style.width = '100%';
+    newDiv.style.display = 'flex';
+    newDiv.style.margin = '0';
+    newDiv.style.padding = '0';
+    let gridContainer = document.querySelector('.grid-container');
+    gridContainer.appendChild(newDiv);
+    
+    for (let j = 0; j < x; j++){
+        innerDiv = document.createElement("div");
+        innerDiv.style.boxSizing = 'border-box';
+        innerDiv.id = 'grid';
+        innerDiv.style.border = '0.5px solid black';            innerDiv.style.flex = '1';
+        innerDiv.style.margin = '0';
+        innerDiv.style.padding = '0';
+        newDiv.appendChild(innerDiv)
+    };
+}
+
+
+
+
+
+
+
+
+
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    reset = document.querySelectorAll('#grid');
+        reset.forEach((grid) => {
+            grid.remove();
+        })
+    x = this.value;
+    for (let i = 0; i < x; i++){
+        newDiv = document.createElement("div");
+        newDiv.id = 'grid';
+        newDiv.style.height = '10px';
+        newDiv.style.boxSizing = 'border-box';
+        newDiv.style.width = '10px';
+        newDiv.style.border = '0.5px solid black';
+        newDiv.style.flex = '1';
+        newDiv.style.width = '100%';
+        newDiv.style.display = 'flex';
+        newDiv.style.margin = '0';
+        newDiv.style.padding = '0';
+        let gridContainer = document.querySelector('.grid-container');
+        gridContainer.appendChild(newDiv);
+        
+        for (let j = 0; j < x; j++){
+            innerDiv = document.createElement("div");
+            innerDiv.style.boxSizing = 'border-box';
+            innerDiv.id = 'grid';
+            innerDiv.style.border = '0.5px solid black';            innerDiv.style.flex = '1';
+            innerDiv.style.margin = '0';
+            innerDiv.style.padding = '0';
+            newDiv.appendChild(innerDiv)
+        };
+    }
+}
+
--- test4/main.js
+++ test4/main.js
@@ -1,13 +1,29 @@
 const btn = document.querySelector('button')
 const container = document.querySelector('#container')
+const newDiv = document.createElement('div');
+
+newDiv.textContent = "Wow i'm new!";
+newDiv.style.color = 'white';
+newDiv.style.alignSelf = 'center';
+
 let click = 0;
 btn.addEventListener('click', ()=>{
     if (click % 2 == 0){
         container.classList.add('new-style');
+        container.appendChild(newDiv);
+        container.style.gap = '10px';
+        
+
         click++;
     }
     else {
         container.classList.remove('new-style');
+        container.removeChild(newDiv);
         click++;
     }
+
+
+
+
 })
+
